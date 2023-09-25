dc3727d4d71ae3a6c9269d8d304503ff89d6721e
pradeepyadav5137 <pradeepapex27@gmail.com>
2023-09-25 15:10:31 +0530

    Background Color Change Project

---
+++ background.js
@@ -0,0 +1,20 @@
+
+const back = document.querySelector(".background") 
+function handleChange(value){
+   back.style. backgroundColor = value;
+}
+
+function handleImage(){
+ const fileInput = document.getElementById("imageInput");
+ const selectedFile = fileInput.files[0];
+
+ if (selectedFile) {
+     const imageURL = URL.createObjectURL(selectedFile);
+     back.style.backgroundImage = `url(${imageURL})`;
+     back.style.backgroundSize = 'cover';
+     back.style.backgroundRepeat = 'no-repeat';
+     back.style.backgroundPosition = 'center center';
+ }
+
+
+}
